30660e622991fc7d1887bfd5bececee44cbc8314
Nekgate <Nekgate@gmail.com>
2024-07-10 14:48:23 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,20 +8,20 @@ to result in exactly n H characters in the file.
 def minOperations(n):
     """Get fewest number of operations needed
     to result in exactly n H characters.
-    
+
     Args:
         n (int): The target number of H characters.
-        
+
     Returns:
         int: The minimum number of operations needed.
     """
     # If n is less than 2, no operations are needed (0 operations)
     if n < 2:
         return 0
-    
+
     # Initialize the number of operations and the current factor (root)
     opertn, root = 0, 2
-    
+
     # Loop until root exceeds n
     while root <= n:
         # If n is divisible by root
@@ -34,6 +34,5 @@ def minOperations(n):
             root -= 1
         # Increment root to check the next possible factor
         root += 1
-    
-    return opertn
 
+    return opertn
