d611404652a530cc401967af57e17c98f1618e70
Nekgate <Nekgate@gmail.com>
2024-07-24 23:11:55 +0100

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,26 +12,25 @@ def validUTF8(data):
     Returns:
         bool: True if data is a valid UTF-8 encoding, else False.
     """
-    numb_bytes = 0
+    num_bytes = 0
 
     for byte in data:
         byte = byte & 0xFF  # Only consider the least significant 8 bits
 
-        if numb_bytes == 0:
+        if num_bytes == 0:
             # Determine how many bytes the current UTF-8 character has
             if (byte >> 5) == 0b110:
-                numb_bytes = 1
+                num_bytes = 1
             elif (byte >> 4) == 0b1110:
-                numb_bytes = 2
+                num_bytes = 2
             elif (byte >> 3) == 0b11110:
-                numb_bytes = 3
+                num_bytes = 3
             elif (byte >> 7):
                 return False
-
         else:
-            # Check that the byte is a continuation byte.
+            # Check that the byte is a continuation byte
             if (byte >> 6) != 0b10:
                 return False
             num_bytes -= 1
 
-    return numb_bytes == 0
+    return num_bytes == 0
